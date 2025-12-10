0f30fa6c2000e48999bce9e6ddcc7446bd3f3938
deepak6600 <34986329+deepak6600@users.noreply.github.com>
2025-12-10 21:11:23 +0530

    casdc
    dad

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCcpgSbizPnAIfza97vwVnjZAQtoug5FuU",
+  authDomain: "famtoolapp-23028.firebaseapp.com",
+  databaseURL: "https://famtoolapp-23028-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "famtoolapp-23028",
+  storageBucket: "famtoolapp-23028.firebasestorage.app",
+  messagingSenderId: "823040432046",
+  appId: "1:823040432046:web:35389ce96ba2bdf73f1a3f",
+  measurementId: "G-SV0900ND91"
+};
